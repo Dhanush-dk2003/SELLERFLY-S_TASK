a06b03b276c4af2481b37035eafc475661987430
Dhanush-dk2003 <kumardhanush730@gmail.com>
2025-07-14 13:23:52 +0530

    update API base URL to localhost for local development; remove unused prismaTest.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,8 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'https://dturn-task.vercel.app',
+  origin: 'http://localhost:3000',
+ // Adjust this to your frontend URL
   credentials: true
 }));
 app.use(cookieParser());
--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://dturn-backend.onrender.com/api',
+  baseURL: 'http://localhost:5000/api',
   withCredentials: true // Important for cookies
 });
 
